63ff603e7df47266de250b1cd718edcda5ede00e
Mestane <erdgn.ilyas@hotmail.com>
2023-06-22 23:17:43 +0300

    init commit

---
+++ src/main/java/com/tutorial/CRM/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.tutorial.CRM.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/init")
+
+public class DemoController {
+
+    @GetMapping
+    public String initPage(Model model) {
+
+        model.addAttribute("theDate", new Date());
+
+        return "welcome";
+
+    }
+
+
+}
